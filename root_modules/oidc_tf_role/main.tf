0605b4728667f9f115154be6ce17bf613e03a5d1
musty <dainmusty@gmail.com>
2025-11-16 23:34:27 +0000

    revised worklow to best practice12

--- root_modules/oidc_tf_role/main.tf
+++ root_modules/oidc_tf_role/main.tf
@@ -9,43 +9,49 @@ module "bootstrap_iam" {
   tf_role_name = "${local.project_name}-tf-${local.env}-role"
 
   tf_role_permissions = [
-    # --- Existing Permissions ---
-    "s3:*",
-    "dynamodb:*",
-    "kinesis:*",
-    "lambda:*",
-    "iam:GetRole",
-    "iam:ListRoles",
-    "iam:PassRole",
-    "iam:GetOpenIDConnectProvider",
-    "cloudwatch:*",
-    "logs:*",
-    "glue:*",
-    "athena:*",
-    "sns:*",
+  # Existing
+  "s3:*",
+  "dynamodb:*",
+  "kinesis:*",
+  "lambda:*",
+  "iam:GetRole",
+  "iam:ListRoles",
+  "iam:PassRole",
+  "iam:GetOpenIDConnectProvider",
+  "cloudwatch:*",
+  "logs:*",
+  "glue:*",
+  "athena:*",
+  "sns:*",
 
-    # --- NEW: Required for Terraform to create IAM resources ---
-    "iam:CreateRole",
-    "iam:DeleteRole",
-    "iam:UpdateRole",
-    "iam:AttachRolePolicy",
-    "iam:DetachRolePolicy",
-    "iam:PutRolePolicy",
-    "iam:DeleteRolePolicy",
-    "iam:CreatePolicy",
-    "iam:CreatePolicyVersion",
-    "iam:DeletePolicyVersion",
-    "iam:GetPolicy",
-    "iam:GetPolicyVersion",
-    "iam:ListPolicyVersions",
-    "iam:TagRole",
+  # IAM - added earlier
+  "iam:CreateRole",
+  "iam:DeleteRole",
+  "iam:UpdateRole",
+  "iam:AttachRolePolicy",
+  "iam:DetachRolePolicy",
+  "iam:PutRolePolicy",
+  "iam:DeleteRolePolicy",
+  "iam:CreatePolicy",
+  "iam:CreatePolicyVersion",
+  "iam:DeletePolicyVersion",
+  "iam:GetPolicy",
+  "iam:GetPolicyVersion",
+  "iam:ListPolicyVersions",
+  "iam:TagRole",
 
-    # --- NEW: Required for EventBridge rules for Lambda triggers ---
-    "events:PutRule",
-    "events:DeleteRule",
-    "events:PutTargets",
-    "events:RemoveTargets"
-  ]
+  # NEW: IAM required for reading inline policies  
+  "iam:ListRolePolicies",
+  "iam:GetRolePolicy",
+
+  # EventBridge required actions
+  "events:PutRule",
+  "events:DeleteRule",
+  "events:PutTargets",
+  "events:RemoveTargets",
+  "events:DescribeRule",
+  "events:ListTargetsByRule"
+]
 
   github_repo_project_link = "repo:dainmusty/stock-market-analytics:*"
 
